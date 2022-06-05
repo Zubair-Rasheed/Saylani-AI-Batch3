59c08144df79f6c546963f4a1446becc052471b2
Zubair <zubairrasheed0316@gmail.com>
2022-06-05 11:49:10 -0700

    Contribution in sylani Batch 3 Group

---
+++ MyIntro.py
@@ -0,0 +1,10 @@
+
+MyIntro = {
+    'id' : '40200',
+    'Name' : 'Zubair Rasheed',
+    'S/O' : 'Abdul Rasheed',
+    'Course' : 'Al & DS',
+    'Batch' : '3',
+    'Class Timing' : '9 to 12 PM',
+    }
+print(MyIntro)
